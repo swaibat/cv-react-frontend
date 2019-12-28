05f4c04bf777cd5fb05e003a4614f88c2a5a726d
Swaibu Rumbiiha <rumbiihas@gmail.com>
2019-12-28 07:25:19 +0200

    feature(configure-webpack):setup production webpack config

--- src/App/Components/breadcrumb.js
+++ src/App/Components/breadcrumb.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
 export default ({ data }) => {
-	console.log('cscscsc', data);
 	return (
 		<nav aria-label='breadcrumb flex-row p-0 text-sm-left font-weight-light '>
 			<ol className='breadcrumb p-0 mt-3 bg-transparent'>
 				<li className='breadcrumb-item'>
 					<a href='/'>Home</a>
 				</li>
-				<li className='breadcrumb-item'>{data && <a href={`/category/${data.name}`}>{data.name}</a>}</li>
-				<li className='breadcrumb-item active' aria-current='page'>
-					{data && data.sub}
-				</li>
-				<select id='inputState' className='filter-nav form-control form-control-sm ml-auto'>
+				{data && data.sub ? (
+					<li className='breadcrumb-item'>{data && <a href={`/category/${data.name}`}>{data.name}</a>}</li>
+				) : (
+					<li className='breadcrumb-item active' aria-current='page'>
+						{data && data.name}
+					</li>
+				)}
+				{data && data.sub && (
+					<li className='breadcrumb-item active' aria-current='page'>
+						{data && data.sub}
+					</li>
+				)}
+
+				{/* <select id='inputState' className='filter-nav form-control form-control-sm ml-auto'>
 					<option value=''>Filter by</option>
 					<option value='700'>700cc</option>
 					<option value='1000'>1000cc</option>
@@ -22,7 +30,7 @@ export default ({ data }) => {
 					<option value='2500'>2500cc</option>
 					<option value='3000'>3000cc</option>
 					<option value='4000'>4000cc</option>
-				</select>
+				</select> */}
 			</ol>
 		</nav>
 	);
--- src/App/Pages/single.item.js
+++ src/App/Pages/single.item.js
@@ -1,11 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Card from '../Components/item.card';
 import constants from '../../redux/constants/index';
 import { singleItems } from '../../redux/actions/items.action';
+import Bread from '../Components/breadcrumb';
+import AdImg from '../../assets/images/img.png';
 
 class Item extends Component {
 	componentDidMount() {
+		console.log(window.location.host.split('.')[0]);
 		const { name } = this.props.match.params;
 		this.props.init();
 		this.props.item(name);
@@ -20,11 +24,34 @@ class Item extends Component {
 		console.log('new', payload);
 		return (
 			<>
-				<div className='container vh-50'>
-					<main className=' row'>
-						<div className='container-fluid row m-auto'>
-							<div className='col-md-10 py-5 bg-secondary vh-50'></div>
-							<div className='col-md-2 py-5 bg-warning'></div>
+				<div className='container'>
+					<Bread data={payload && this.props.match.params} />
+					<main className='d-flex'>
+						<div className='item-holder d-flex w-100'>
+							<div className='col-md-2 bg-warning'></div>
+							<div className='col-md-7'>{/* <img className='h-100 w-100 px-1' src={AdImg} alt='img' /> */}</div>
+							<div className='col-md-3 bg-warning'>
+								<div class='card'>
+									<img class='card-img-top' src='...' alt='cap' />
+									<div class='card-body'>
+										<h5 class='card-title'>Card title</h5>
+										<p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+									</div>
+									<ul class='list-group list-group-flush'>
+										<li class='list-group-item'>Cras justo odio</li>
+										<li class='list-group-item'>Dapibus ac facilisis in</li>
+										<li class='list-group-item'>Vestibulum at eros</li>
+									</ul>
+									<div class='card-body'>
+										<a href='#' class='card-link'>
+											Card link
+										</a>
+										<a href='#' class='card-link'>
+											Another link
+										</a>
+									</div>
+								</div>
+							</div>
 						</div>
 					</main>
 				</div>
